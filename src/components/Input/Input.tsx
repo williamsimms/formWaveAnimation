6f2142f4647afc991cc2112ed70da83f5679dd48
williamsimms <williamsimms193@gmail.com>
2020-12-09 10:01:27 -0500

    fixes

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useRef, useEffect } from 'react'
 import './Input.scss'
 
 interface InputProps {
@@ -9,10 +9,23 @@ interface InputProps {
 }
 
 const Input = ({ label, placeholder, required, type }: InputProps) => {
+  const labelRef = useRef<any>()
+
+  useEffect(() => {
+    labelRef.current.innerHTML = labelRef.current.innerText
+      .split('')
+      .map((letter: any, index: number) => {
+        return `<span style="transition-delay:${index * 50}ms">${letter}</span>`
+      })
+      .join('')
+  }, [])
+
+  console.log(labelRef)
+
   return (
     <div className='form__control'>
-      <input type={type} required={required} placeholder={placeholder} className='form__control__input' />
-      <label htmlFor='' className='form__control__label'>
+      <input type={type} required={required} className='form__control__input' />
+      <label className='form__control__label' ref={labelRef}>
         {label}
       </label>
     </div>
